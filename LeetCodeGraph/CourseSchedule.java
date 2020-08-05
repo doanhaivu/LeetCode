27853658e05d470e56560c25536c4a70a6edf905
Vu Doan <doanhaivu@gmail.com>
2020-08-05 14:02:25 -0700

    Course Schedule

--- LeetCodeGraph/CourseSchedule.java
+++ LeetCodeGraph/CourseSchedule.java
@@ -10,6 +10,8 @@ import static org.hamcrest.Matchers.is;
 public class CourseSchedule {
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if (numCourses == 0 || prerequisites.length == 0)
+            return true;
         Map<Integer, ArrayList<Integer>> edges = new HashMap<>();
         Map<Integer, Integer> inDegrees = new HashMap<>();
 
@@ -40,10 +42,22 @@ public class CourseSchedule {
 
         if (aQueue.size() == 0 && numCourses > 0)
             return false;
+        int count = 0;
         while (!aQueue.isEmpty()) {
             Integer aSource = aQueue.pop();
-
-
+            count++;
+            if (count == numCourses)
+                return true;
+            tmp = edges.get(aSource);
+            if (tmp != null) {
+                for (Integer aVertex:tmp) {
+                    int newInDegree = inDegrees.get(aVertex)-1;
+                    if (newInDegree == 0) {
+                        aQueue.offer(aVertex);
+                    }
+                    inDegrees.put(aVertex, newInDegree);
+                }
+            }
         }
         return false;
     }
@@ -52,5 +66,6 @@ public class CourseSchedule {
     public void test() {
         assertThat(canFinish(2, new int[][]{{1,0}}), is(true));
         assertThat(canFinish(2, new int[][]{{1,0}, {0,1}}), is(false));
+        assertThat(canFinish(3, new int[][]{{1,0}}), is(true));
     }
 }
