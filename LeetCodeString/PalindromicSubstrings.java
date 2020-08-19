ac71265826f918724f9cfae742504afff3bbb3df
Vu <doanhaivu@gmail.com>
2020-08-19 13:01:09 -0700

    Palindromic Substrings

--- LeetCodeString/PalindromicSubstrings.java
+++ LeetCodeString/PalindromicSubstrings.java
@@ -10,24 +10,11 @@ public class PalindromicSubstrings {
     public int countSubstrings(String s) {
         if (s.length()==0)
             return 0;
-        boolean[][] dp = new boolean[s.length()][s.length()];
         int ret = 0;
         for (int i = 0; i < s.length(); i++) {
             for (int j = i; j < s.length(); j++) {
-                if (dp[i][j]) continue;
-                if (isPalindrome(s.substring(i,j+1))) {
+                if (isPalindrome(s.substring(i,j+1)))
                     ret++;
-                    while (i >= 1 && j < s.length()-1) {
-                        //System.out.println("aaa");
-                        i--;
-                        j++;
-                        if (s.charAt(i) == s.charAt(j)) {
-                            dp[i][j] = true;
-                            ret++;
-                        } else break;
-                    }
-                }
-
             }
         }
 
